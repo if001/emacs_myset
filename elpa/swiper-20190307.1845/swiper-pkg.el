8d570ba18b2f32428b24d95fc2818ed3ffe5b58d
issei.fujimoto <i.fujimoto@accelia.net>
2019-09-26 17:22:17 +0900

    elpa

--- elpa/swiper-20190307.1845/swiper-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "swiper" "20190307.1845" "Isearch with an overview. Oh, man!" '((emacs "24.1") (ivy "0.11.0")) :commit "6f0f32fe374df2e3e356bdef83d27e71efa79231" :keywords '("matching") :authors '(("Oleh Krehel" . "ohwoeowho@gmail.com")) :maintainer '("Oleh Krehel" . "ohwoeowho@gmail.com") :url "https://github.com/abo-abo/swiper")
