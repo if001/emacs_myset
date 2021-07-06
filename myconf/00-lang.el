db943bdcf376369a63b541d80378736041999f76
issei <fujimoto>
2021-07-06 23:14:16 +0900

    fix: add lang settings for linux

--- myconf/00-lang.el
+++ myconf/00-lang.el
@@ -1,20 +1,45 @@
-;; フォント設定
-;; (if (string-match "ac171.local" system-name)
-;;     )
-(set-face-attribute 'default nil
-		    :family "Ricty Diminished"
-					;: family "Osaka"
-					;: family "M+ 2p"
-		    :height 130)
-(set-fontset-font
- nil 'japanese-jisx0208
- (font-spec :family "Hiragino Kaku Gothic ProN" :size 9))
+;;; 00-lang.el --- Font settings:
 
+;;; Commentary:
 
-;; ;; 英語と日本語の比率を1：2に設定
-(add-to-list 'face-font-rescale-alist
-             '(".*Hiragino Kaku Gothic ProN.*" . 1.2))
+;;; Code:
+(if (string-match "issei-All-Series" (system-name))
+    (progn
+      (message "linux settings")
+      (set-face-attribute 'default nil
+			  :family "Ricty Diminished"
+			  :height 110)
+      (set-fontset-font nil 'japanese-jisx0208 (font-spec :family "Ricty Diminished" :size 14))
+    
+      ;; 英語と日本語の比率を1：2に設定
+      ;; (setq face-font-rescale-alist '((".*Ricty Diminished.*" . 1.2)))
+      )
+  )
+
+(if (string-match "ac171.local" (system-name))
+    (progn
+          (message "ac171.local settings")
+    (set-face-attribute 'default nil
+			:family "Ricty Diminished"
+			:height 130)
+  (set-fontset-font
+   nil 'japanese-jisx0208
+   (font-spec :family "Hiragino Kaku Gothic ProN" :size 9))
+  ;; 英語と日本語の比率を1：2に設定
+  (add-to-list 'face-font-rescale-alist
+	       '(".*Hiragino Kaku Gothic ProN.*" . 1.2))
+      )
+  )
+
 
 ;; 行間を指定
 (setq-default line-spacing 0.1)
 
+;; ずれ確認用
+;; abcdefgchjk あ
+;; 0123456789012345678901234567890123456789
+;; ｱｲｳｴｵｱｲｳｴｵｱｲｳｴｵｱｲｳｴｵｱｲｳｴｵｱｲｳｴｵｱｲｳｴｵｱｲｳｴｵ
+;; あいうえおあいうえおあいうえおあいうえお
+
+
+;;; 00-lang.el ends here
