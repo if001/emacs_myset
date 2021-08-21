f56500e237d8a8eae549331f68c70b2ff5d342e3
issei <fujimoto>
2021-08-22 05:19:55 +0900

    fix: theme

--- myconf/50-doomtheme.el
+++ myconf/50-doomtheme.el
@@ -1,24 +1,62 @@
-;;; doom-theme
-(require 'doom-themes)
-:config
-;; Global settings (defaults)
-(setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
-      doom-themes-enable-italic t) ; if nil, italics is universally disabled
+;;; 50-doomtheme.el --- doomtheme-mode settings:
 
-;; Load the theme (doom-one, doom-molokai, etc); keep in mind that each theme
-;; may have their own settings.
-(load-theme 'doom-one t)
+;;; Commentary:
 
-;; Enable flashing mode-line on errors
-(doom-themes-visual-bell-config)
+;;; Code:
+(use-package doom-themes
+  :custom
+  (doom-themes-enable-italic t)
+  (doom-themes-enable-bold t)
+  :custom-face
+  (doom-modeline-bar ((t (:background "#6272a4"))))
+  :config
+  (load-theme 'doom-dracula t)
+  (doom-themes-neotree-config)
+  (doom-themes-org-config)
+  
+  ;; tab-lineの設定をここで上書き4
+  (set-face-attribute 'tab-line nil ;; background behind tabs
+		      :background "#2c2f33"
+		      :family "Ricty Diminished"
+		      :height 1.0 :box nil)
+  (set-face-attribute 'tab-line-tab nil ;; active tab in another window
+		      :inherit 'tab-line
+		      :background "grey20"
+		      :foreground "#DCDCCC"
+		      :box nil)
+  (set-face-attribute 'tab-line-tab-current nil ;; active tab in current window
+		      :background "grey50"
+		      :foreground "#DCDCCC"
+		      :box nil)
+  (set-face-attribute 'tab-line-tab-inactive nil ;; inactive tab
+		      :background "grey20"
+		      :foreground "#DCDCCC"
+		      :box nil)
+  (set-face-attribute 'tab-line-highlight nil ;; mouseover
+		      :background "white"
+		      :foreground 'unspecified)
+)
+    
+;; (require 'doom-themes)
+;; :config
+;; ;; Global settings (defaults)
+;; (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
+;;       doom-themes-enable-italic t) ; if nil, italics is universally disabled
 
-;; Enable custom neotree theme (all-the-icons must be installed!)
-(doom-themes-neotree-config)
-;; or for treemacs users
-(doom-themes-treemacs-config)
+;; ;; Load the theme (doom-one, doom-molokai, etc); keep in mind that each theme
+;; ;; may have their own settings.
+;; (load-theme 'doom-one t)
 
-;; Corrects (and improves) org-mode's native fontification.
-(doom-themes-org-config)
+;; ;; Enable flashing mode-line on errors
+;; (doom-themes-visual-bell-config)
+
+;; ;; Enable custom neotree theme (all-the-icons must be installed!)
+;; (doom-themes-neotree-config)
+;; ;; or for treemacs users
+;; (doom-themes-treemacs-config)
+
+;; ;; Corrects (and improves) org-mode's native fontification.
+;; (doom-themes-org-config)
 
 ;; 左の行番号の色設定はdoomのを上書きするためここに記述
 ;; (set-face-attribute 'linum nil
@@ -29,14 +67,24 @@
 
 ;; -------------------------------------------------------------------
 ;; doom-modeline
-(use-package doom-modeline)
-(doom-modeline-mode 1)
+    (use-package doom-modeline
+      :custom
+      (doom-modeline-buffer-file-name-style 'truncate-with-project)
+      (doom-modeline-icon t)
+      (doom-modeline-major-mode-icon t)
+      (doom-modeline-minor-modes nil)
+      (doom-modeline-lsp t)
+      (doom-modeline-github t)
+      
+      :hook
+      (after-init . doom-modeline-mode)
+      :config
+      (line-number-mode 0)
+      (column-number-mode 0)
+      (doom-modeline-def-modeline 'main
+    '(bar workspace-number window-number evil-state god-state ryo-modal xah-fly-keys matches buffer-info remote-host buffer-position parrot selection-info)
+    '(misc-info persp-name lsp github debug minor-modes input-method major-mode process vcs checker)))
 
-;; How tall the mode-line should be (only respected in GUI Emacs).
-(setq doom-modeline-height 25)
-
-;; How wide the mode-line bar should be (only respected in GUI Emacs).
-(setq doom-modeline-bar-width 3)
 
 ;; Determines the style used by `doom-modeline-buffer-file-name'.
 ;;
@@ -55,75 +103,7 @@
 ;; If you are expereicing the laggy issue, especially while editing remote files
 ;; with tramp, please try `file-name' style.
 ;; Please refer to https://github.com/bbatsov/projectile/issues/657.
-(setq doom-modeline-buffer-file-name-style 'truncate-upto-project)
-
-;; What executable of Python will be used (if nil nothing will be showed).
-;;(setq doom-modeline-python-executable "python")
-
-;; Whether show `all-the-icons' or not (if nil nothing will be showed).
-(setq doom-modeline-icon t)
-
-;; Whether show the icon for major mode. It respects `doom-modeline-icon'.
-(setq doom-modeline-major-mode-icon t)
-
-;; Display color icons for `major-mode'. It respects `all-the-icons-color-icons'.
-(setq doom-modeline-major-mode-color-icon nil)
-
-;; Whether display minor modes or not. Non-nil to display in mode-line.
-(setq doom-modeline-minor-modes nil)
-
-;; If non-nil, a word count will be added to the selection-info modeline segment.
-(setq doom-modeline-enable-word-count t)
-
-;; If non-nil, only display one number for checker information if applicable.
-(setq doom-modeline-checker-simple-format t)
-
-;; Whether display perspective name or not. Non-nil to display in mode-line.
-(setq doom-modeline-persp-name t)
-
-;; Whether display `lsp' state or not. Non-nil to display in mode-line.
-(setq doom-modeline-lsp t)
-
-;; Whether display github notifications or not. Requires `ghub` package.
-(setq doom-modeline-github t)
-
-;; The interval of checking github.
-(setq doom-modeline-github-interval (* 30 60))
-
-;; Whether display environment version or not.
-(setq doom-modeline-env-version t)
-
-;; Whether display mu4e notifications or not. Requires `mu4e-alert' package.
-(setq doom-modeline-mu4e t)
-
-
-;; tabの設定
-(set-face-attribute 'tabbar-default nil
-		    :background "#2c2f33"
-		    ;; :background "grey20"
-		    ;:family "Ricty Diminished"
-		    :height 1.0
-		    :box nil)
-(set-face-attribute 'tabbar-selected nil
-		    :background "grey50"
-		    :foreground "#DCDCCC"
-		    :box nil)
-(set-face-attribute 'tabbar-unselected nil
-		    ;; :background "grey20"
-		    :background "#2c2f33"
-		    :foreground "#DCDCCC"
-		    :box nil)
-(set-face-attribute 'tabbar-separator nil
-		    ;; :background "grey20"
-		    :background "#2c2f33"
-		    :box nil)
-(set-face-attribute 'tabbar-button nil
-		    :box nil)
-(set-face-attribute 'header-line nil
-		    :background "#2c2f33"
-		    ;; :background "grey20"
-		    :box nil)
-
+;; (setq doom-modeline-buffer-file-name-style 'truncate-upto-project)
 
 ;;; is large file
 (defun is-current-file-size-small (size)
@@ -135,9 +115,11 @@
 ;; インデントハイライト
 (use-package highlight-indent-guides
   :ensure t
+  :custom
+  (highlight-indent-guides-method 'column)
+  ;; (highlight-indent-guides-method 'character)
+  (highlight-indent-guides-responsive t)
   :config
-  (setq highlight-indent-guides-method 'column)
-  (setq highlight-indent-guides-responsive t)
   (defun toggle-indent ()
     (interactive)
     (if (is-current-file-size-small 10000)
@@ -150,10 +132,10 @@
   )
 
 
-;;-------------------------------------------------------------------------;;
-(use-package hiwin
-  :config;; 非アクティブウィンドウの背景色を設定
-  (hiwin-activate)                           ;; hiwin-modeを有効化
-  (set-face-background 'hiwin-face "#20232a") ;; 非アクティブウィンドウの背景色を設定
-  )
-;;-------------------------------------------------------------------------;;
+;; ;;-------------------------------------------------------------------------;;
+;; (use-package hiwin
+;;   :config;; 非アクティブウィンドウの背景色を設定
+;;   (hiwin-activate)                           ;; hiwin-modeを有効化
+;;   (set-face-background 'hiwin-face "#20232a") ;; 非アクティブウィンドウの背景色を設定
+;;   )
+;; ;;-------------------------------------------------------------------------;;
