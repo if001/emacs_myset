b41d7a29618f51a6ff0deb8e726514c706853572
if001 <otomijuf.004@gmail.com>
2025-07-02 03:17:53 +0900

    un use use-package

--- init.el
+++ init.el
@@ -18,19 +18,9 @@
 (require 'profiler)
 ;; (profiler-start 'cpu)
 
-(defun add-to-load-path (&rest paths)
-  (let (path)
-    (dolist (path paths paths)
-      (let ((default-directory (expand-file-name (concat user-emacs-directory path))))
-        (add-to-list 'load-path default-directory)
-        (if (fboundp 'normal-top-level-add-subdirs-to-load-path)
-            (normal-top-level-add-subdirs-to-load-path))))))
-
 ;; デフォルトでensure t
-(use-package use-package
-  :config
-  (setq use-package-always-ensure t))
-
+(setq use-package-always-ensure t)
+  
 ;; load api key
 (let ((secrets-file (expand-file-name "secrets.el" user-emacs-directory)))
   (when (file-exists-p secrets-file)
@@ -38,7 +28,8 @@
 
 (add-to-list 'load-path (expand-file-name "site-lisp/init-loader/" user-emacs-directory))
 (require 'init-loader)
-(init-loader-load "~/.emacs.d/myconf")
+(setq init-loader-byte-compile t)
+(init-loader-load "~/.emacs.d/myconf/")
 
 
 (provide 'init)
@@ -49,9 +40,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(blink-cursor-mode nil)
- '(inhibit-startup-screen t)
- '(package-selected-packages nil)
- '(show-paren-mode t))
+ '(package-selected-packages nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
